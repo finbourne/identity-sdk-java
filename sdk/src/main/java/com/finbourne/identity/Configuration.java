85edc649dd2731de522ab725ba9ed47f7f6cdbef
Concourse <concourse@finbourne.com>
2025-02-25 11:32:29 +0000

    identity java updates for v2.0.161
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/identity/Configuration.java
+++ sdk/src/main/java/com/finbourne/identity/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.identity;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.157";
+    public static final String VERSION = "2.0.161";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
